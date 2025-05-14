ca417e5e15b9ddf1eb3ffd5139eeabb925d3383c
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2025-05-14 15:30:30 -0300

    Add feature to list image pre-orders via new API endpoint
    Introduced a new API route `/api/pre-order/image` to fetch image-based pre-orders. Updated the sidebar to include a link for "Pré-pedido via imagem" and implemented the required service and controller logic.

--- src/ClientApp/src/components/layout/SidebarMenu.vue
+++ src/ClientApp/src/components/layout/SidebarMenu.vue
@@ -43,6 +43,10 @@ import RecentChats from '../sidebar/RecentChats.vue';
                   <i class="bi bi-chat-square-dots px-4"></i>
                   <span>Meus pedidos</span>
                 </RouterLink>
+                <RouterLink to="/send-image-pre-order" class="dropdown-item">
+                  <i class="bi bi-chat-square-dots px-4"></i>
+                  <span>Pré-pedido via imagem</span>
+                </RouterLink>
                 <RouterLink to="/info-chat" class="dropdown-item">
                   <i class="bi bi-chat-square-dots px-4"></i>
                   <span>Assistente de informações</span>
@@ -61,10 +65,6 @@ import RecentChats from '../sidebar/RecentChats.vue';
                 <RouterLink to="/orders" class="dropdown-item">
                   <i class="bi bi-chat-square-dots px-4"></i>
                   <span>Todos os pedidos</span>
-                </RouterLink>
-                <RouterLink to="/send-image-pre-order" class="dropdown-item">
-                  <i class="bi bi-chat-square-dots px-4"></i>
-                  <span>Enviar pré-pedido via imagem</span>
                 </RouterLink>
                 <RouterLink to="/orders-chat" class="dropdown-item">
                   <i class="bi bi-chat-square-dots px-4"></i>
